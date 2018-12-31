cacd2fc257cfa4a709340258c0906b0265ff37dc
wulinlide <695160225@qq.com>
2018-12-31 19:27:40 +0800

    final

--- Final Project/吴琳丽-161220140/huluwa1/src/main/java/com/company/Creature/Cheer.java
+++
@@ -1,8 +0,0 @@
-package com.company.Creature;
-import com.company.BattleField.Position;
-import com.company.Creature.Creature;
-
-import java.util.*;
-public interface Cheer {
-    void creatureCheer(Position[][] Field, int x, int y);//小写
-}
--- Final Project/吴琳丽-161220140/huluwa1/src/main/java/com/company/Formation/Formation.java
+++
@@ -1,9 +0,0 @@
-package com.company.Formation;
-
-import com.company.BattleField.Position;
-import com.company.Creature.Creature;
-
-public interface Formation {
-    boolean ifEmpty(Position[][] Field,int x,int y);
-    void setCreatures(Position[][] Field,int x,int y, Creature[] creatures);
-}
